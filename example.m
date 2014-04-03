b6f68caf59c35720218f953e2a82e9ecfd1dda70
Wei Shang <david.w.shang@gmail.com>
2014-04-02 21:29:24 -0300

    initial commit
    zoomPlot function now returns the handle of the zoom axes so further
    manipulation (e.g., labels, ticks, etc) can be made outside of the
    function.

---
+++ example.m
@@ -0,0 +1,15 @@
+close all; clear all; clc;
+
+figure; 
+t = 0:0.001:0.1;
+plot(t, cos(2*pi*50*t)); 
+ah = gca; 
+% location of the plot to be zoomed in.  
+s_pos =[0.05 0 0.06 0.1];
+% location of the zoom-in plot  
+t_pos = [0.035 0.4 0.065 0.7];    
+
+% generate a zoom-in plot.  
+az = zoomPlot(ah, s_pos, t_pos);     
+
+xlabel(az,'time (s)');
