07fc28954a1b68ec5f2fc983238dc7c3a44e6439
Wei Shang <david.w.shang@gmail.com>
2014-04-27 22:40:57 -0300

    add screenshot

--- example.m
+++ example.m
@@ -3,6 +3,8 @@ close all; clear all; clc;
 figure; 
 t = 0:0.001:0.1;
 plot(t, cos(2*pi*50*t)); 
+xlabel('time t (s)');
+ylabel('cos(2 \pi 50 t)');
 ah = gca; 
 % location of the plot to be zoomed in.  
 s_pos =[0.05 0 0.06 0.1];
@@ -12,4 +14,5 @@ t_pos = [0.035 0.4 0.065 0.7];
 % generate a zoom-in plot.  
 az = zoomPlot(ah, s_pos, t_pos);     
 
-xlabel(az,'time (s)');
+% can manipulate the added axes 
+% xlabel(az,'time (s)');
